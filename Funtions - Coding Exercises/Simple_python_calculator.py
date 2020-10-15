8b69bbb89e69003c5be3ebca00682165aae7e3e0
OmaroVega <omarovegagonzalez@gmail.com>
2020-10-15 15:39:09 +0100

    Simple python calculator

---
+++ Funtions - Coding Exercises/Simple_python_calculator.py
@@ -0,0 +1,42 @@
+# Python Program - Sumple Python Calculator
+
+def add(num1,num2):
+    result = num1 + num2
+    print('El resultado es: ' + str(result))
+
+def subs(num1,num2):
+    result = num1 - num2
+    print('El resultado es: ' + str(result))
+
+def mul(num1,num2):
+    result = num1 * num2
+    print('El resultado es: ' + str(result))
+
+def div(num1,num2):
+    result = num1 / num2
+    print('El resultado es: ' + str(result))
+
+print('1. Suma')
+print('2. Resta')
+print('3. Multiplicación')
+print('4. División')
+print('5. Salir')
+
+while True:
+    choice = int(input('Introduce tu elección: '))
+    if choice >= 1 and choice <=4:
+        print('Introduce dos números')
+        num1 = int(input('Introduce el primer número: '))
+        num2 = int(input('Introduce el segundo número: '))
+        if choice == 1:
+            add(num1,num2)
+        elif choice == 2:
+            subs(num1,num2)
+        elif choice == 3:
+            mul(num1,num2)
+        else:
+            div(num1,num2)
+    elif choice == 5:
+        break
+    else:
+        print('Has introducido un número incorrecto.')
